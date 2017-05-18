b447bb355fb23ee1d058b968a49179ade1a0e1db
Keehong Youn <younkeehong@gmail.com>
2017-05-18 14:31:38 -0700

    background

--- graphics.cpp
+++ graphics.cpp
@@ -194,7 +194,8 @@ struct AlloApp : OmniStereoGraphicsRenderer1 {
   }
 
   virtual void onDraw(Graphics& g) {
-    omni().clearColor() = Color(0, 0, 0);
+    // omni().clearColor() = Color(0, 0, 0);
+    omni().clearColor() = Color(0.0f, omni().face() / 5.0f, (5.0f - omni().face()) / 5.0f);
 
     glActiveTexture(GL_TEXTURE0);
     // glEnable(GL_TEXTURE_3D);
